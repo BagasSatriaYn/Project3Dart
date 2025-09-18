9f371f693da37c852f9c4798a39c71067df44e84
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-09-18 20:14:33 +0700

    equality dart

---
+++ bin/equality.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  int a = 11;
+  int b = 10;
+  print(a == b); // false karena 11 != 10
+
+  String s1 = "Dart";
+  String s2 = "Dart";
+  print(s1 == s2); // true karena string literal sama
+}
